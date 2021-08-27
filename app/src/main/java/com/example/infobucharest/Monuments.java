23f8bf404c21e36903e1eddac8736e3cb72f6a04
Roberto <nanuroberto20@gmail.com>
2021-08-27 20:28:08 +0300

    update 2

---
+++ app/src/main/java/com/example/infobucharest/Monuments.java
@@ -0,0 +1,14 @@
+package com.example.infobucharest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Monuments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monuments);
+    }
+}
